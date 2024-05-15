e3dc2d1b840583f746005e5b82865669ff15c4b1
Swapnil Dubey <swapnildubey285@gmail.com>
2024-05-15 23:45:12 +0530

    basic changes

---
+++ server/env.js
@@ -0,0 +1,14 @@
+// require('dotenv').config(); // Load environment variables from .env file
+
+// const env = {
+//     MONGODB_USERNAME: 'swapnildubey',
+//     MONGODB_PASSWORD:'sneakin_database_23',
+
+//     REACT_APP_GOOGLE_CLIENT_ID: 232858920977-llov97dcc4rp542aqj84teeillhgdf0t.apps.googleusercontent.com
+//   // Add other environment variables you need
+// };
+
+// module.exports = env;
+
+
+
